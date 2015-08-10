53b2c98a6d3c38fe8d510c6c2cd62b5353715091
Arthur <arthur.4lves@gmail.com>
2015-08-10 20:06:43 -0300

    Create retropyssion.py

---
+++ retropyssion.py
@@ -0,0 +1,24 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.image import Image
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.properties import ObjectProperty
+
+class ScreenController(ScreenManager):
+	background = ObjectProperty(Image(source="bg.jpg"))
+
+
+class MainScreen(Screen):
+    background = ObjectProperty(Image(source="select_bar.png"))
+
+
+class OtherScreen(Screen):
+    pass
+
+
+class MainApp(App):
+    def build(self):
+        return ScreenController()
+
+if __name__ == "__main__":
+    MainApp().run()
