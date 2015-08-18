1dceccfe430f3644c0d03d5b6a4b7729fc0228fd
Arthur Alves <arthur.4lves@gmail.com>
2015-08-17 23:03:52 -0300

    Add some textures

--- animate_test/anime.py
+++ animate_test/anime.py
@@ -5,6 +5,8 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.core.window import Window
 from kivy.properties import ObjectProperty
 from kivy.clock import Clock
+from sprite import Sprite
+from functools import partial
 
 Window.clearcolor = [0, 0, 0, 0]
 
@@ -12,18 +14,19 @@ Window.clearcolor = [0, 0, 0, 0]
 class ScreenController(ScreenManager):
     pass
 
+
 class MainScreen(Screen):
-    sprite = ObjectProperty(None)
-    sprite_id = 0
+    sprite = Sprite(sprite_sheet={"walk": [0, 1, 2, 3]})
+    # sprite = ObjectProperty(None)
+    # sprite_id = 0
 
-    def anime(self, delta):
-        if self.sprite_id > 3:
-            self.sprite_id = 0
-        self.sprite.source = "atlas://../piu/testatlas/{}".format(self.sprite_id)
-        self.sprite_id += 1
+    # def anime(self, delta):
+    #     if self.sprite_id > 3:
+    #         self.sprite_id = 0
+    #     self.sprite.source = "atlas://../piu/testatlas/{}".format(self.sprite_id)
+    #     self.sprite_id += 1
 
-    def update(self):
-        Clock.schedule_interval(self.anime, 0.2)
+    Clock.schedule_interval(partial(sprite.play, "walk"), 0.2)
 
 
 class AnimeApp(App):
